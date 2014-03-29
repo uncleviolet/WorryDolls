91538040ec046140a3cee379c17ee39cf5f81217
uncleviolet <ian_tillbill@hotmail.com>
2014-03-29 20:24:52 +0000

    Start drag implementation

--- js/Doll.js
+++ js/Doll.js
@@ -5,22 +5,60 @@ var Doll = function(sprite, x, y, scale) {
 	this.scale = scale;
 	this.width = this.scale*this.sprite.width;
 	this.height = this.scale*this.sprite.height;
-	this.speedX = 0.05;
-	this.speedY = 0.05;
+	this.dragExpandTime = 80;
 };
 
 Doll.prototype.update = function(progress) {
-	if (this.x <= 0.5*this.width || this.x >= this.mainCanvas.width - 0.5*this.width) {
-		this.speedX *= -1;
+	if (this.isBeingDragged) {
+		this.getDragPosition();
+		this.getDragSize(progress);
 	}
-	if (this.y <= 0.5*this.height || this.y >= this.mainCanvas.height - 0.5*this.height) {
-		this.speedY *= -1;
-	}	
-	this.x += progress*this.speedX;
-	this.y += progress*this.speedY;
-
 };
 
 Doll.prototype.draw = function() {
-	this.ctx.drawImage(this.sprite, this.x-0.5*this.width, this.y-0.5*this.height, this.width, this.height);
+	this.ctx.drawImage(this.sprite, this.x - 0.5*this.width, this.y - 0.5*this.height, this.width, this.height);
+};
+
+Doll.prototype.isUnderTouchCoords = function(x, y) {
+	if (x < this.x + 0.5*this.width && x > this.x - 0.5*this.width &&
+		y < this.y + 0.5*this.height && y > this.y - 0.5*this.height) {
+		return true;
+	}
+	return false;
+};
+
+Doll.prototype.initDrag = function(x, y) {
+	this.isBeingDragged = true;
+	main.dragItem = this;
+	this.dragOffsetX = x - this.x;
+	this.dragOffsetY = y - this.y;
+	this.isExpanding = true;
+	this.timeSpentExpanding = 0
+}
+
+Doll.prototype.endDrag = function() {
+	this.isBeingDragged = false;
+	this.width = this.scale*this.sprite.width;
+	this.height = this.scale*this.sprite.height;
+};
+
+Doll.prototype.getDragPosition = function() {
+	if (main.mouseIsDown) {
+		this.x = main.mouseX - this.dragOffsetX;
+		this.y = main.mouseY - this.dragOffsetY;
+	}
+};
+
+Doll.prototype.getDragSize = function(progress) {
+	if (main.mouseIsDown) {
+		this.timeSpentExpanding += progress;
+		var scaleFactor = 1.2;
+		if (this.timeSpentExpanding < this.dragExpandTime) {
+			scaleFactor = 1.0 + 0.2*(this.timeSpentExpanding/this.dragExpandTime);
+		}
+		this.width = scaleFactor*this.scale*this.sprite.width;
+		this.height = scaleFactor*this.scale*this.sprite.height;
+	} else {
+
+	}
 };
--- js/Main.js
+++ js/Main.js
@@ -1,17 +1,29 @@
 var Main = function() {
 	this.mainCanvas = document.getElementById("mainCanvas");
+	this.mainCanvas.height = 640;//window.innerHeight;
+	this.mainCanvas.width = 400;//window.innerWidth;
 	this.ctx = this.mainCanvas.getContext('2d');
 	this.ctx.fillStyle = "rgb(255,255,255)";
 	var main = this;
+
+	this.mouseIsDown = false;
+	this.enableMouseEvents = true;
+	this.mouseX = 0;
+	this.mouseY = 0;
+
 	var doll1Sprite = new Image();
 	var doll1X = 200.0;
 	var doll1Y = 200.0;
 	doll1Sprite.src = 'img/doll1.png';
 	doll1Sprite.addEventListener("load", function() {
-		main.doll1 = new Doll(doll1Sprite, doll1X, doll1Y, 0.5);
-		main.doll1.main = main;
-		main.doll1.ctx = main.ctx;
-		main.doll1.mainCanvas = main.mainCanvas;
+		var doll1 = new Doll(doll1Sprite, doll1X, doll1Y, 0.3);
+		doll1.main = main;
+		doll1.ctx = main.ctx;
+		doll1.mainCanvas = main.mainCanvas;
+
+		main.displayList = [doll1];
+
+		main.initMouseEvents();
 
 
 		var oldTimestamp = null;	
@@ -31,15 +43,59 @@ var Main = function() {
 
 		
 	}, false);
-
-
 };
 
 Main.prototype.update = function(progress) {
-	this.doll1.update(progress);
+	for (var i = 0; i < this.displayList.length; i++) {
+		this.displayList[i].update(progress);
+	}
 };
 
 Main.prototype.draw = function() {
 	this.ctx.fillRect(0.0, 0.0, this.mainCanvas.width, this.mainCanvas.height);
-	this.doll1.draw();
+	for (var i = 0; i < this.displayList.length; i++) {
+		this.displayList[i].draw();
+	}
+};
+
+Main.prototype.initMouseEvents = function() {
+	main = this;
+	function mouseDown(e) {
+		e.preventDefault();
+		if (main.enableMouseEvents) {
+			main.mouseIsDown = true;
+			mouseXY(e);
+			if (main.displayList[0].isUnderTouchCoords(main.mouseX, main.mouseY)) {
+				main.displayList[0].initDrag(main.mouseX, main.mouseY);
+			}
+		}
+	};
+	function mouseXY(e) {
+		if (main.enableMouseEvents) {
+			main.mouseX = e.pageX - main.mainCanvas.getBoundingClientRect().left;
+			main.mouseY = e.pageY - main.mainCanvas.getBoundingClientRect().top;
+		}
+	};
+	function mouseUp(e) {
+		if (main.enableMouseEvents) {
+			main.mouseIsDown = false;
+			if (main.dragItem != null) {
+				main.dragItem.endDrag();
+				main.dragItem = null;
+			}
+		}
+	};
+	//document.body.style.webkitUserSelect='none';
+		
+	this.mainCanvas.addEventListener("mousedown", mouseDown, false);
+	this.mainCanvas.addEventListener("mousemove", mouseXY, false);
+	document.body.addEventListener("mouseup", mouseUp, false);
+
+	document.addEventListener("touchmove", function(e) { e.preventDefault(); }, false);
+	this.mainCanvas.addEventListener("touchstart", mouseDown, false);
+	this.mainCanvas.addEventListener("touchmove", mouseXY, false);	
+	this.mainCanvas.addEventListener("touchend", function(e) {	
+		mouseUp();
+	}, false);
+	document.body.addEventListener("touchcancel", mouseUp, false);
 };
